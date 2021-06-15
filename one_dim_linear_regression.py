0719a0f44bdb51cdc44a50759474dc6e95a5c407
Hooiu <lycosloyal@naver.com>
2021-06-15 15:05:21 +0900

    1차 선형 회귀

--- one_dim_linear_regression.py
+++ one_dim_linear_regression.py
@@ -1,3 +1,19 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+train_dataset = [65, 50, 55, 65, 55, 70, 65, 70]
+train_labels = [85, 74, 76, 90, 85, 87, 94, 98]
+
+def build_model():
+  model = keras.Sequential(tf.keras.layers.Dense(1,input_dim = 1))
+  optimizer = tf.keras.optimizers.SGD(learning_rate=0.0001)
+
+  model.compile(loss='mse',
+                optimizer=optimizer)
+  return model
+
+model = build_model()
+
+model.fit(train_dataset,train_labels,epochs=1000)
+
+print(model.predict(np.array([70])))
